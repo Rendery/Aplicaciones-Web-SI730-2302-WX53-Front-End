4feba229ebb4db4503160623863bfd7ed658a64e
nreupomckinsey <naldo_reupo-musayon@mckinsey.com>
2023-09-04 20:43:49 -0500

    Feature(LibraryCenter) i18n separte file

--- src/components/tutorial-component.vue
+++ src/components/tutorial-component.vue
@@ -1,5 +1,21 @@
 <template>
     <label class="title">{{$t('tutorial')}}  {{title}}</label>
+    <div class="form-container">
+        <div class="control">
+            <label >Description :</label>
+            <input type="text" >
+        </div>
+        <div class="control">
+            <label>Count :</label>
+            <input type="number">
+        </div>
+        <div class="control">
+            <label>Date :</label>
+            <input type="date">
+        </div>
+        <button>Save</button>
+    </div>
+
 </template>
 
 <script>
@@ -15,4 +31,19 @@ export default {
 .title{
     font-style: italic;
 }
+
+.form-container{
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+}
+.control{
+    display: flex;
+    flex-direction: row;
+    gap: 4px;
+}
+.form-container label{
+    width: 100px;
+}
+
 </style>
